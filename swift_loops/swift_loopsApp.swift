174364b9569765f17c4016be76e5bd53240e9611
ZachMommaerts <zmommaerts@gmail.com>
2023-03-04 22:15:16 -0600

    Initial Commit

---
+++ swift_loops/swift_loopsApp.swift
@@ -0,0 +1,17 @@
+//
+//  swift_loopsApp.swift
+//  swift_loops
+//
+//  Created by Zach Mommaerts on 3/4/23.
+//
+
+import SwiftUI
+
+@main
+struct swift_loopsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
