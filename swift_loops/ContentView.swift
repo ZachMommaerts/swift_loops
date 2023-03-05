03b666260a357233adb4b4996e7c1c74a9a97afb
ZachMommaerts <zmommaerts@gmail.com>
2023-03-04 22:46:55 -0600

    finished for loop challenge functionality

--- swift_loops/ContentView.swift
+++ swift_loops/ContentView.swift
@@ -8,12 +8,40 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var ints = [Int]()
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            
+            List(ints, id: \.self){ int in
+                Text(String(int))
+            }
+            
+            Button(action: {
+                var int:Int
+                repeat{
+                    let randInt = Int.random(in: 1...10)
+                    int = randInt
+                    ints.append(randInt)
+                } while(int != 7)
+                
+            }, label: {
+                Text("Add integer")
+            })
+            
+            Button(action: {
+                for i in ints.indices{
+                    ints[i] += 1
+                }
+            }, label: {
+                Text("Increase integers")
+            })
+            
+            Button(action: {
+                ints.removeAll()
+            }, label: {
+                Text("Clear integers")
+            })
         }
         .padding()
     }
